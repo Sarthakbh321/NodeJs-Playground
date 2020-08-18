7c8368b6c0541a7743871317415d512e2864df64
Sarthak <sarthakbh321@gmail.com>
2020-08-19 00:45:20 +0530

    used async/await for saving message

--- routes/messages.js
+++ routes/messages.js
@@ -7,20 +7,19 @@ router.get("/", (req, res) => {
 	res.send("We are on messages");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 	console.log(req.body);
 
 	const message = new Message({
 		text: req.body.text
 	});
 	
-	message.save()
-	.then((data) => {
-		res.status(200).send(data);
-	})
-	.catch((error) => {
-		res.json({message: error});
-	});
+	try {
+		const saveMessage = await message.save();
+		res.status(200).json(saveMessage);
+	} catch(error) {
+		res.json({message: error})
+	}
 });
 
 module.exports = router;
