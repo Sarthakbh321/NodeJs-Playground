08f121ff1e972880ebbba736e7d41cc89398e082
Sarthak <sarthakbh321@gmail.com>
2020-08-18 23:55:29 +0530

    added mongoDB connection

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-	res.send("Server is up and running");
-})
+	res.send(`Server is up and running ${process.env.USER_NAME}`);
+});
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true}, () => {
+	console.log("Connected to db!");
+});
 
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
