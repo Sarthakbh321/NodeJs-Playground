2159da75a46710108291dc6bab67adddc0041a76
Sarthak <sarthakbh321@gmail.com>
2020-08-19 00:42:41 +0530

    message saved to db

--- routes/messages.js
+++ routes/messages.js
@@ -9,6 +9,18 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
 	console.log(req.body);
+
+	const message = new Message({
+		text: req.body.text
+	});
+	
+	message.save()
+	.then((data) => {
+		res.status(200).send(data);
+	})
+	.catch((error) => {
+		res.json({message: error});
+	});
 });
 
 module.exports = router;
